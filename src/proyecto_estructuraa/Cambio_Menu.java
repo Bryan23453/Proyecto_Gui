76f8ccf22e0e7abc3ee9cccfe6114660fa277be0
Bryan23453 <rayan.andino@gmail.com>
2023-09-20 12:54:03 -0600

    modificaciones logos y agregar paneles faltantes

--- src/proyecto_estructuraa/Cambio_Menu.java
+++ src/proyecto_estructuraa/Cambio_Menu.java
@@ -5,10 +5,12 @@
 package proyecto_estructuraa;
 
 import java.awt.Label;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 /**
  *
@@ -17,35 +19,37 @@ import javax.swing.JPanel;
 
 public class Cambio_Menu extends Thread {
     JPanel panel;
-    JPanel panel2;
-    JPanel panel3;
     JPanel panel4;
     JPanel panel5;
     JPanel panel6;
     JPanel panel7;
+    JTabbedPane tabpane1;
+    JTabbedPane tabpanel2;
+    boolean pass;
     JLabel a;
-    boolean pass=false;
-    public Cambio_Menu(JPanel pane,JPanel pane2,JPanel pane3,JPanel pane4,JPanel pane5,JPanel pane6,JLabel a,JPanel pane7){
+    public Cambio_Menu(JPanel pane,JPanel pane4,JPanel pane5,JPanel pane6,JLabel a,JPanel pane7,JTabbedPane tabpane,JTabbedPane tabpane2,boolean pas){
         panel=pane;
-        panel2=pane2;
-        panel3=pane3;
         panel4=pane4;
         panel5=pane5;
         panel6=pane6;
         panel7=pane7;
         this.a=a;
+        tabpane1=tabpane;
+        tabpanel2=tabpane2;
+        pass=pas;
         
     }
     
     public void run () {
-        a.setEnabled(false);
-        int w=1230;
-        panel2.setLocation(w, 0);
-        panel3.setLocation(w, 0);
-        panel4.setLocation(w, 0);
-        panel5.setLocation(w, 0);
-        panel6.setLocation(w, 0);
-        panel7.setLocation(w, 0);
+        if (pass) {
+            a.setEnabled(false);
+            int w=1230;
+            tabpane1.setLocation(w, 0);
+            tabpanel2.setLocation(w, 0);
+            panel4.setLocation(w, 0);
+            panel5.setLocation(w, 0);
+            panel6.setLocation(w, 0);
+            panel7.setLocation(w, 0);
             while (w>80){
             try {
                     Thread.sleep(1);
@@ -55,17 +59,27 @@ public class Cambio_Menu extends Thread {
                 
             }
             a.setEnabled(true);
-            a.repaint();
-            
-    }
-    
-    public boolean pasoo(){
-        if (pass) {
-            return true;
         }else{
-            return false;
+            a.setEnabled(false);
+            int w=1230;
+            panel.setLocation(w,0);
+            tabpanel2.setLocation(w, 0);
+            panel4.setLocation(w, 0);
+            panel5.setLocation(w, 0);
+            panel6.setLocation(w, 0);
+            panel7.setLocation(w, 0);
+            while (w>80){
+            try {
+                    Thread.sleep(1);
+                    tabpane1.setLocation(w,0);
+                    w=w-3;
+                } catch (InterruptedException ex) {}
+                
+            }
+            a.setEnabled(true);
         }
         
+            
     }
     
 }
